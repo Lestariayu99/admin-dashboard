b0309818b8a2da6ddef2928f31edd0368ee67517
Isabella Lois <lestariayu0801@gmail.com>
2024-06-03 21:51:41 +0700

    Add dashboard page

---
+++ app/pages/dashboard.js
@@ -0,0 +1,10 @@
+// pages/dashboard.js
+export default function Dashboard() {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        <p>Selamat datang di halaman dashboard!</p>
+      </div>
+    );
+  }
+  
